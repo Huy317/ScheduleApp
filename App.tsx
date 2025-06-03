2eaa8e49e892504f6774e263ce7725006b696af8
Nguyen Gia Huy <ghgaming1234563@gmail.com>
2025-06-03 08:54:31 +0700

    added login screen

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import ToDoList from './src/ToDoScreen/ToDoList';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import AddToDo from './src/ToDoScreen/AddToDo';
-import AddProfile from './src/ProfileScreen/AddProfile';
+import Login from './src/LoginRegister/Login';
 
 const Stack = createStackNavigator();
 const TodoScreen = () => {
@@ -13,7 +13,7 @@ const TodoScreen = () => {
     <Stack.Navigator
       screenOptions={{
         headerStyle: {
-          backgroundColor: 'green', //TODO: Change this to theme color
+          backgroundColor: '#7F55B1',
         },
         headerTintColor: '#fff',
         headerTitleStyle: {
@@ -21,7 +21,7 @@ const TodoScreen = () => {
         },
       }}
     >
-      <Stack.Screen name="ToDo List" component={ToDoList}
+      <Stack.Screen name="Task List" component={ToDoList}
         options={({ navigation }) => ({
           headerRight: () => (
             <TouchableOpacity
@@ -42,18 +42,14 @@ const TodoScreen = () => {
   );
 }
 
-const ProfileScreen = () => {
-  return(
-    <AddProfile />
-  )
-}
+
 
 const TestingScreen = () => {
   // Change this to test each screen
   // will be removed and replace with bottom tab navigator later
   // when all screens are ready
   return (
-    <ProfileScreen />
+    <Login />
   )
 }
 
---
+++ src/LoginRegister/Login.js
@@ -0,0 +1,143 @@
+import React, { useState } from "react";
+import { StyleSheet, TextInput, TouchableOpacity, View, Text, Alert } from "react-native";
+
+const Login = ({ }) => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = () => {
+        if (email.trim() === "" || password.trim() === "") {
+            Alert.alert("Input Error", "Email and Password cannot be empty");
+            return;
+        }
+        if (password.trim().length < 8) {
+            Alert.alert("Password too short", "Password must be at least 6 characters long");
+            return;
+        }
+        console.log("Login attempted with:", { email, password });
+        // Reset fields after login attempt
+        setEmail("");
+        setPassword("");
+    }
+    const handleRegister = () => {
+        
+        console.log("Navigate to Register Screen");
+    }
+    const handleForgotPassword = () => {
+       
+        console.log("Navigate to Forgot Password Screen");
+    }
+    return (
+        <View style={styles.container}>
+            <Text style={styles.appTitle}>Simple Task</Text>
+            {/* <Text style={styles.loginText}>Login</Text> */}
+            <TextInput
+                placeholder="Email"
+                onChangeText={setEmail}
+                style={styles.input}
+                value={email}
+            />
+            <TextInput
+                placeholder="Password"
+                secureTextEntry={true}
+                onChangeText={setPassword}
+                style={styles.input}
+                value={password}
+            />
+            <TouchableOpacity
+                style={styles.forgotSection}
+                onPress={handleForgotPassword}
+            >
+                <Text style={styles.forgotText}>Forgot Password?</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+                onPress={handleLogin}
+                style={styles.button}
+            >
+                <Text style={styles.buttonText}>Login</Text>
+            </TouchableOpacity>
+
+            <View style={styles.registerSection}>
+                <Text style={styles.dontHaveAccountText}>Don't have an account?</Text>
+                <TouchableOpacity
+                    onPress={handleRegister}
+                    
+                >
+                    <Text style={styles.registerText}>   Register</Text>
+                </TouchableOpacity>
+            </View>
+        </View >
+    )
+}
+
+export default Login;
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 10,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#F2F2F2",
+    },
+    appTitle: {
+        fontSize: 34,
+        fontWeight: "bold",
+        textAlign: "center",
+        marginBottom: 20,
+        color: "#7F55B1",
+    },
+    loginText: {
+        fontSize: 24,
+        fontWeight: "bold",
+        marginBottom: 20,
+        color: "#DA6C6C",
+    },
+    input: {
+        width: "80%",
+        padding: 10,
+        borderWidth: 1,
+        borderColor: "#ccc",
+        borderRadius: 5,
+        marginBottom: 15,
+        backgroundColor: "#fff",
+    },
+    button: {
+        backgroundColor: "#007BFF",
+        padding: 15,
+        borderRadius: 5,
+        alignItems: "center",
+        width: "80%",
+    },
+    buttonText: {
+        color: "#fff",
+        fontSize: 16,
+        fontWeight: "bold",
+    },
+    forgotText: {
+        color: "#007BFF",
+        marginBottom: 10,
+        textDecorationLine: "underline",
+        fontSize: 16,
+    },
+    forgotSection: {
+        width: "80%",
+        alignItems: "flex-end",
+        marginBottom: 15,
+    },
+    dontHaveAccountText: {
+        color: "#888",
+        fontSize: 16,
+    },
+    registerText: {
+        color: "#007BFF",
+        fontSize: 16,
+    },
+    registerSection:{
+        width: "80%",
+        flexDirection: "row",
+        justifyContent: "center",
+        marginTop: 20,
+    }
+
+})
